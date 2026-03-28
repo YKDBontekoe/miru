8cd406bc48d68ffd8b801c47bd68e6a01f310a2c
Youri Bontekoe <youribontekoe@gmail.com>
2026-03-28 09:22:20 +0100

    Delete patch_chat2.tsx

--- patch_chat2.tsx
+++
@@ -1,9 +0,0 @@
---- frontend/app/(main)/chat.tsx
-+++ frontend/app/(main)/chat.tsx
-@@ -16,6 +16,7 @@
- import { useTranslation } from 'react-i18next';
- import { AppText } from '../../src/components/AppText';
- import { useChatStore } from '../../src/store/useChatStore';
-+import { ApiService } from '../../src/core/api/ApiService';
- import { useAgentStore } from '../../src/store/useAgentStore';
- import { ChatRoom, Agent } from '../../src/core/models';
